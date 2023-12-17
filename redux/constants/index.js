fc5c97afda795d02f81e0531c875361c3aa026a3
pankt <pankti.courses@gmail.com>
2023-12-17 08:06:15 +0100

    initial commit

--- redux/constants/index.js
+++ redux/constants/index.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://10.0.2.2:8080/api/v1';
+export const BASE_URL = 'https://c998-2409-40f2-8-28ba-58a7-f5e7-5004-999.ngrok-free.app/api/v1';
 export const GET_CONFIG = 'GET_CONFIG';
 export const GET_SHOTS_VIEW_REST_SORTING_CONFIG =
     'GET_SHOTS_VIEW_REST_SORTING_CONFIG';
